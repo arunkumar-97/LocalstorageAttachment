d2e4f01aa6ef7ced498a384c9f82ccf7e698dc21
Guest_User <arunkumar88967@gmail.com>
2020-10-27 18:09:11 +0530

    new1

--- src/main/java/com/jesperapps/api/controller/EmployeeProfilePictureController.java
+++ src/main/java/com/jesperapps/api/controller/EmployeeProfilePictureController.java
@@ -1,17 +1,20 @@
 package com.jesperapps.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.jesperapps.api.model.EmployeeProfilePicture;
 import com.jesperapps.api.service.AttachmentService;
 
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(AttachmentService.BASE_URL)
 public class EmployeeProfilePictureController {
@@ -23,10 +26,12 @@ public class EmployeeProfilePictureController {
 	@GetMapping("/{pictureId}")
 	ResponseEntity viewProfilePicture(@PathVariable Integer pictureId) {
 		EmployeeProfilePicture pictureFromDB = this.attachmentService.getByPictureId(pictureId);
-		if(pictureFromDB != null) {
+		if(pictureFromDB != null) {	
 			return ResponseEntity.status(HttpStatus.OK)
-					.contentType(pictureFromDB.isJPGPicture() ? MediaType.IMAGE_JPEG : MediaType.IMAGE_PNG)
-					.body(attachmentService.getFileBytes(pictureFromDB.getPictureName()));
+//					.contentType(MediaType)
+					.header("Content-Type", "video/mp4")
+//					.header("Content-Disposition", "attachment; filename="+pictureFromDB.getPictureName())
+					.body(new ByteArrayResource(attachmentService.getFileBytes(pictureFromDB.getPictureName())));
 		}
 		return ResponseEntity.status(HttpStatus.OK).body("No picture found");
 	}
