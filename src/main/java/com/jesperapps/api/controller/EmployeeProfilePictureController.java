82d51865001a78702d4647bc92590610c47b1f08
Guest_User <arunkumar88967@gmail.com>
2020-10-28 17:52:38 +0530

    changed the gif image

--- src/main/java/com/jesperapps/api/controller/EmployeeProfilePictureController.java
+++ src/main/java/com/jesperapps/api/controller/EmployeeProfilePictureController.java
@@ -3,7 +3,7 @@ package com.jesperapps.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,12 +26,21 @@ public class EmployeeProfilePictureController {
 	@GetMapping("/{pictureId}")
 	ResponseEntity viewProfilePicture(@PathVariable Integer pictureId) {
 		EmployeeProfilePicture pictureFromDB = this.attachmentService.getByPictureId(pictureId);
-		if(pictureFromDB != null) {	
+		if(pictureFromDB != null) {
+		if(pictureFromDB.getPictureName().toLowerCase().contains("mp4")) {
 			return ResponseEntity.status(HttpStatus.OK)
-//					.contentType(MediaType)
+
 					.header("Content-Type", "video/mp4")
+
+					.body(new ByteArrayResource(attachmentService.getFileBytes(pictureFromDB.getPictureName())));
+		}else {
+			return ResponseEntity.status(HttpStatus.OK)
+					.header("Content-Type", "png/jpeg")
+					.contentType(MediaType.IMAGE_JPEG)
 //					.header("Content-Disposition", "attachment; filename="+pictureFromDB.getPictureName())
 					.body(new ByteArrayResource(attachmentService.getFileBytes(pictureFromDB.getPictureName())));
+		}
+		
 		}
 		return ResponseEntity.status(HttpStatus.OK).body("No picture found");
 	}
